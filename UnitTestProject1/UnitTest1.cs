98936bbcde599f0835ec62de183d20e00d16a0dd
andpoulsen <and.poulsen@gmail.com>
2018-03-21 15:32:25 +0100

    Using shims

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,22 +1,42 @@
- using System;
- using ConsoleApp1;
- using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pose;
+using Shim = Pose.Shim;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        private Shim _todayShim;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _todayShim = Shim.Replace(() => DateTime.Today).With(() => new DateTime(2018, 3, 21));
+        }
+
         [TestMethod]
         public void TestYesterday()
         {
-            Assert.IsTrue(DateService.WasYesterDay(new DateTime(2018, 3, 20)));
+            bool? result = null;
+            PoseContext.Isolate(() =>
+                {
+                    result = DateService.WasYesterDay(new DateTime(2018, 3, 20));
+                }, _todayShim);
+            Assert.IsTrue(result != null && result.Value);
         }
 
         [TestMethod]
         public void TestYesterdayEvening()
         {
-            Assert.IsTrue(DateService.WasYesterDay(new DateTime(2018, 3, 20, 23, 0, 0)));
+            bool? result = null;
+            PoseContext.Isolate(() =>
+            {
+                result = DateService.WasYesterDay(new DateTime(2018, 3, 20, 23, 0, 0));
+            }, _todayShim);
+            Assert.IsTrue(result!=null && result.Value);
         }
     }
 }
