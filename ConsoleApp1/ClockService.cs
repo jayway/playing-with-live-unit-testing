710e8ae8b9dd06387e58c95957a6ff2bee9649b3
andpoulsen <and.poulsen@gmail.com>
2018-03-22 11:51:13 +0100

    Adding ClockService

---
+++ ConsoleApp1/ClockService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public interface IClockService
+    {
+        DateTime Now { get; }
+    }
+    public class ClockService : IClockService
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
