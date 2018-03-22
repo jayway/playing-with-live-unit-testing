1c84ef7c8b34e16f615d0823565b240ca920f989
andpoulsen <and.poulsen@gmail.com>
2018-03-22 14:23:24 +0100

    Testing with Mocks

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,43 +1,59 @@
 using System;
 using ConsoleApp1;
-using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
 {
-    class ClockServiceForTesting : IClockService
-    {
-        public DateTime Now => new DateTime(2018, 3, 21);
-    }
-
     [TestClass]
     public class UnitTest1
     {
-        private ServiceProvider _serviceProvider;
+        private readonly Mock<IClockService> _clockServiceMock = new Mock<IClockService>();
         private DateService _dateService;
         
         [TestInitialize]
         public void Setup()
         {
-            _serviceProvider = new ServiceCollection()
-                .AddSingleton<IClockService, ClockServiceForTesting>()
-                .AddSingleton<DateService>()
-                .BuildServiceProvider();
-
-            _dateService = _serviceProvider.GetService<DateService>();
+            _clockServiceMock.Setup(c => c.Now).Returns(new DateTime(2018, 3, 21));
+            _dateService = new DateService(_clockServiceMock.Object);
         }
 
         [TestMethod]
         public void TestYesterday()
         {
             Assert.IsTrue(_dateService.WasYesterDay(new DateTime(2018, 3, 20)));
+            _clockServiceMock.VerifyGet(c => c.Now, Times.Exactly(1));
         }
 
         [TestMethod]
         public void TestYesterdayEvening()
         {
-
             Assert.IsTrue(_dateService.WasYesterDay(new DateTime(2018, 3, 20, 23, 0, 0)));
+            _clockServiceMock.VerifyGet(c => c.Now, Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void TestFarIntoTheFuture1()
+        {
+            _clockServiceMock.Setup(c => c.Now).Returns(DateTime.MaxValue);
+            Assert.IsFalse(_dateService.WasYesterDay(new DateTime(2018, 3, 20)));
+            _clockServiceMock.VerifyGet(c => c.Now, Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void TestFarIntoTheFuture2()
+        {
+            _clockServiceMock.Setup(c => c.Now).Returns(DateTime.MaxValue);
+            Assert.IsTrue(_dateService.WasYesterDay(new DateTime(9999, 12, 30)));
+            _clockServiceMock.VerifyGet(c => c.Now, Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void TestTheBeginningOfTime()
+        {
+            _clockServiceMock.Setup(c => c.Now).Returns(DateTime.MinValue);
+            Assert.IsFalse(_dateService.WasYesterDay(new DateTime(2018, 3, 20)));
+            _clockServiceMock.VerifyGet(c => c.Now, Times.Exactly(1));
         }
     }
 }
